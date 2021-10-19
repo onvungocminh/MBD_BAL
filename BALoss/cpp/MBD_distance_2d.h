8039223d71c5ed3e8002fcc8ae5caf2c36ea2eb6
Minh On Vu Ngoc <movn@lrde.epita.fr>
2021-10-19 21:13:24 +0200

    update loss function

--- BALoss/cpp/MBD_distance_2d.h
+++ BALoss/cpp/MBD_distance_2d.h
@@ -9,15 +9,5 @@ struct Point2D
     int h;
 };
 
-
-void MBD_waterflow(const unsigned char * img, const unsigned char * seeds, unsigned char * distance, 
-    int height, int width);
-
-void geodesic_shortest(const unsigned char * img, const unsigned char * seeds, const unsigned char * destination, unsigned char * shortestpath, 
-                              int height, int width);
-
-void geodesic_shortest_all(const unsigned char * img, const unsigned char * seeds, const unsigned char * destination, unsigned char * shortestpath, 
-                              int height, int width);
-
 void MBD_cut(const unsigned char * img, const unsigned char * seeds, const unsigned char * destination, unsigned char * distance, 
                               int height, int width);
