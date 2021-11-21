ce90d2b155e1ec785e7f9a3a766778e1053ee280
Minh On Vu Ngoc <movn@lrde.epita.fr>
2021-11-21 18:02:03 +0100

    update demo

---
+++ BALoss/demo.py
@@ -0,0 +1,36 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import cv2
+import MBD
+from loss import *
+import scipy.io
+from PIL import Image
+import pdb
+
+
+img_file = 'img.png'
+img = Image.open(img_file)
+img = np.array(img, dtype=np.float32)
+seed_file = 'seed.mat'
+seed = scipy.io.loadmat(seed_file)["seed"]
+boundary_file = 'boundary.mat'
+boundary = scipy.io.loadmat(boundary_file)["boundary"]	
+contour_file = 'contour.png'
+contour = Image.open(contour_file)
+contour = contour.convert('L')
+contour = np.array(contour)/255.
+contour = contour.astype(np.uint8)
+
+
+img = torch.from_numpy(img.copy()).float()
+img = img.unsqueeze(axis=0)
+contour = torch.from_numpy(np.array([contour])).float()
+seed = torch.from_numpy(np.array([seed])).squeeze(axis=0).float()
+boundary = torch.from_numpy(np.array([boundary])).squeeze(axis=0).float()
+
+img, contour, seed, boundary = img.cuda(),  contour.cuda(), seed.cuda(), boundary.cuda()
+
+BAloss = DAHU_loss(img, seed, boundary, contour)
+print(BAloss)
--- BALoss/loss.py
+++ BALoss/loss.py
@@ -27,29 +27,22 @@ def cross_entropy_loss2d(inputs, targets, cuda=True, balance=1.1):
     return loss
 
 
-def MBDcut_loss(inputs, seed, labels):
-    """    
-    : inputs:  edge probability map
-    : seed: initializated seed for inside and outside regions 
-    : labels: boundary for each region 
-    """
-
+def DAHU_loss(inputs, seed, boundary, labels):
+    # target_image: the boundary total
     EPM = inputs.squeeze()
     labels = labels.squeeze()
 
     seed = seed.squeeze(axis = 0).squeeze(axis = 0)
+    boundary = boundary.squeeze(axis=0).squeeze(axis = 0)
 
     d, _, _ = seed.shape
     loss_total = 0
     for i in range(d):
         loss = 0
-        EPM_s, seed_s = EPM, seed[i,:,:]
-
+        EPM_s, seed_s, boundary_s = EPM, seed[i,:,:], boundary[i,:,:]
         max_range = torch.max(seed_s).type(torch.uint8)
         seed_s = np.array(seed_s.detach().cpu().numpy()).astype(np.uint8)
-
         EPM_s = torch.sigmoid(EPM_s)
-
         EPM_s_clone = EPM_s.clone()
         EPM_s_clone = np.array((EPM_s_clone*255).detach().cpu().numpy()).astype(np.uint8)
 
@@ -63,7 +56,7 @@ def MBDcut_loss(inputs, seed, labels):
             contour = cv2.dilate(contour,kernel,iterations = 1)
             contour = (contour/255).astype(np.uint8)
             contour = torch.from_numpy(np.array([contour])).cuda()
-                      
+            
             EPM_contour = (EPM_s * contour).unsqueeze(axis=0)
             label_s = (labels * contour).unsqueeze(axis=0)
             BCE_loss = cross_entropy_loss2d(EPM_contour, label_s, True, 1.1)*10
@@ -73,4 +66,5 @@ def MBDcut_loss(inputs, seed, labels):
             loss = torch.tensor(0).type(torch.float64)
         loss_total = loss_total + loss # Accumulate sum
     loss_total = loss_total / d
+
     return loss_total
